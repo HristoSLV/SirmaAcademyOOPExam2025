c6c2de2852ea657f1178ce24cf5837641a128145
HristoSLV <hristoivanovslavchev@gmail.com>
2025-07-06 23:54:48 +0300

    add services

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import controller.MainController;
-import service.CarService;
-import service.CustomerService;
-import service.RentalService;
-
 public class Main {
     public static void main(String[] args) {
 
